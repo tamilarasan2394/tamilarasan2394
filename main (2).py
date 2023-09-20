032509fa374ebd22c773b2082e6fc4e9e14b6895
tamilarasan2394 <144437853+tamilarasan2394@users.noreply.github.com>
2023-09-20 12:38:58 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,19 @@
+# Leap year
+
+"""
+year % 4 == 0 &
+year % 100 != 0 /
+year % 400 == 0
+"""
+def isLeapYear(year):
+ if(year % 4 == 0 and year % 100 !=0) or year % 400 == 0:
+      return True
+ else:
+    return False
+
+year = 2013
+
+if isLeapYear(year):
+    print('{} is a ieap year .'.capitalizeformat(year))
+else:
+   print('{} is not a leap year.'.format(year))
